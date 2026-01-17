4e944d76ee3f0c0aa49dd2ef62d01936866317a9
Anastus <johnanastus25@gmail.com>
2026-01-17 11:36:55 +0530

    Intial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ padding: "24px" }}>
+      <h1>CSV Supplier Heatmap – ProQSmart Assignment</h1>
+    </div>
+  );
+}
+
+export default App;
